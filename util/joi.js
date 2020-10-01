7f56f2c1e52761987259855fff8fed53676b4843
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2020-10-01 11:58:39 +0700

    Add helpers

--- util/joi.js
+++ util/joi.js
@@ -1,5 +1,20 @@
 import JoiOriginal from 'joi'
 
-export default JoiOriginal.defaults((schema) => {
+const Joi = JoiOriginal.defaults((schema) => {
   return schema.options({ presence: 'required' })
+}).extend((schema) => {
+  return {
+    type: 'mongoid',
+    base: schema.string().length(24)
+  }
 })
+
+Joi.DBObject = function (fields) {
+  return Joi.object(Object.assign({
+    _id: Joi.string(),
+    createdAt: Joi.date(),
+    updatedAt: Joi.date(),
+  }, fields))
+}
+
+export default Joi
